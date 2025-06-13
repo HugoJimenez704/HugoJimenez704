8332f46cd2675475086c4b767c3b0a44fdd9b475
HugoJimenez704 <jlhugoa@gmail.com>
2025-06-13 13:27:11 -0400

    memoclock

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const projectsContainer = document.getElementById('projects-container');
 
-fetch('./projects.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(project => {
-            const projectElement = document.createElement('div');
-            projectElement.classList.add('project');
+fetch('projects.json')
+  .then(res => res.json())
+  .then(projects => {
+    projects.forEach((proj, i) => {
+      const projectElement = document.createElement('div');
+      projectElement.className = 'project';
+      projectElement.style.animationDelay = `${i * 0.1 + 0.2}s`;
 
-            projectElement.innerHTML = `
-                <h2>${project.title}</h2>
-                <p>${project.description}</p>
-                <img src="${project.image}" alt="${project.title}">
-                ${project.video ? `<iframe width="560" height="315" src="${project.video}" frameborder="0" allowfullscreen></iframe>` : ''}
-            `;
+      let media = '';
+      if (proj.image) {
+        media = `<img src="${proj.image}" alt="Project image"/>`;
+      } else if (proj.youtube) {
+        media = `<iframe src="https://www.youtube.com/embed/${proj.youtube}" allowfullscreen></iframe>`;
+      }
 
-            projectsContainer.appendChild(projectElement);
-        });
-    })
-    .catch(error => console.error('Error loading projects:', error));
+      projectElement.innerHTML = `
+        ${media}
+        <h2>${proj.title}</h2>
+        <p>${proj.description}</p>
+        ${proj.link ? `<a href="${proj.link}" target="_blank">View Project</a>` : ''}
+      `;
+
+      projectsContainer.appendChild(projectElement);
+    });
+  })
+  .catch(error => console.error('Error loading projects:', error));
